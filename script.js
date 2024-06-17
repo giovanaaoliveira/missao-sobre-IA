2ca685abd0338e7a2798c08bc0e6a99e95277779
giovanaaoliveira <126570714+giovanaaoliveira@users.noreply.github.com>
2024-06-17 14:40:13 +0000

    alteracoes no codigo

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal= document.querySelector(`.caixa-principal`);
+const caixaPerguntas= document.querySelector(`caixa-perguntas`);
+const caixaAlternativas= document.querySelector(`caixa-alternativas`);
+const caixaResultados= document.querySelector(`caixa-resultado`);
+const textoResultados= document.querySelector(`texto-resultado`);
+const perguntas=[
+    {
+        enunciado:`pergunta 1`,
+        alternativas: [
+            `alternativa 1`,
+            `alternativa 2`
+        ]
+    }
+]
